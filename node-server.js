929584cbd1c4923273c7fb214c833cacd436bf06
katreyna <kathyareynosa@gmail.com>
2025-09-10 22:43:42 +0000

    https node server

---
+++ node-server.js
@@ -0,0 +1,19 @@
+const http = require('http');
+const availableTimes = {
+    Monday: ["1:00", "1:30", "2:00", "2:30", "3:00", "3:30", "4:00", "4:30"],
+    Tuesday: ["1:00", "1:30", "2:00", "2:30", "3:00", "3:30", "4:00", "4:30"],
+    Wednesday: ["1:00", "1:30", "2:00", "2:30", "3:00", "4:00", "4:30"],
+    Thursday: ["1:00", "1:30", "2:00", "2:30", "3:00", "3:30", "4:00", "4:30"],
+    Friday: ["1:30", "2:00", "2:30", "3:00", "3:30", "4:00", "4:30"],
+};
+const appointments = [
+    {name: "James", day: "Wednesday", time: "3:30" },
+    {name: "Lillie", day: "Friday", time: "1:00" }];
+
+let serverObj = http.createServer(function(req,res){
+	res.writeHead(200, {'content-type':'text/plain'});
+	res.write('recieved');
+	res.end();
+});
+
+serverObj.listen(80, function(){console.log("listening on port 80")});
