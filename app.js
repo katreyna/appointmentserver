26e21bed833a5aac26243abb60586ea130507fdc
katreyna <kathyareynosa@gmail.com>
2025-09-22 22:23:50 +0000

    finished version of appointment scheduling server

--- app.js
+++ app.js
@@ -22,33 +22,97 @@ let serverObj =  http.createServer(function(req,res){
 		case "/cancel":
 			cancel(urlObj.query,res);
 			break;
+		case "/check":
+			check(urlObj.query,res);
+			break;
 		default:
 			error(res,404,"pathname unknown");
 
 	}
 });
 
+// helper
+
+function sendRes(res, status, message) {
+	res.writeHead(200,{'content-type':'text/plain'});
+        res.write(message);
+        res.end();
+}
+
+function check(qObj, res) {
+	
+	//check if day is valid
+	
+	if (!availableTimes[qObj.day]) {
+                error(res, 400, "Invalid Day");
+		return;
+
+	// check if timeslot is valid
+
+	if (availableTimes[qObj.day].some(time => time === qObj.time)) {
+		sendRes(res, 200, "Timeslot is available!");
+	} else {
+		sendRes(res, 200, "Timeslot is not available!");
+	}
+}
+
 function schedule(qObj,res) {
 	if (availableTimes[qObj.day].some(time => time == qObj.time))
 	{
-		res.writeHead(200,{'content-type':'text/plain'});
-		res.write("scheduled");
-		res.end();
+		// remove time
+		availableTimes[qObj.day] = availableTimes[qObj.day].filter(time => time!= qObj.time);
+
+		// add new appointment
+
+		appointments.push({
+			name: qObj.name,
+			day: qObj.day,
+			time: qObj.time
+		});
+
+		sendRes(res, 200, "scheduled!");
 	}
-	else 
-		error(res,400,"Can't schedule");
+	else
+		error(res,400,"Can't schedule, appointment time not available");
 
- 
+
 }
 
-function cancel(qObj) {
+function cancel(qObj, res) {
+
+	// find index of array
+
+	const appointmentIndex = appointments.findIndex(appt => 
+		appt.name === qObj.name &&
+		appt.day === qObj.day &&
+		appt.time == qObj.time
+	);
+
+	// remove  and add back appt
+
+        if (appointmentIndex !== -1)
+        {
+		// remove 
+		const removedAppointment = appointments.splice(appointmentIndex,1);	
+
+		// adding back
+
+		if (availableTimes[qObj.day]) {
+			availableTimes[qObj.day].push(qObj.time);
+			availableTimes[qObj.day].sort();
+		}
+
+		sendRes(res, 200, "Appointment Cancelled");
+	
+	} else {
+		error(res, 404, "Appointment Not Found");
+	}
 }
 
-function error(response,status,message) {
+function error(res, status, message) {
+	
+	sendRes(res, status, message);
 
-	response.writeHead(status,{'content-type':'text/plain'});
-	response.write(message);
-	response.end();
 }
 
 serverObj.listen(80,function(){console.log("listening on port 80")});
